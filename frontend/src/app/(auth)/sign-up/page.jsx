f14508dbab1fc9ab35eec6adb35ccb4769f09375
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-15 16:33:44 +0100

    styled signup page in mobile

--- frontend/src/app/(auth)/sign-up/page.jsx
+++ frontend/src/app/(auth)/sign-up/page.jsx
@@ -1,3 +1,9 @@
+import SignUpForm from "@/app/components/forms/SignUpForm";
+
 export default function SignUpPage() {
-  return <p>Sign Up</p>;
+  return (
+    <section className="px-4 h-[90vh] flex flex-col justify-center">
+      <SignUpForm />
+    </section>
+  );
 }
--- frontend/src/app/components/forms/SignUpForm.jsx
+++ frontend/src/app/components/forms/SignUpForm.jsx
@@ -1,26 +1,27 @@
 import Link from "next/link";
 
-import FormButton from "../ui/FormButton";
 import FormInput from "./FormInput";
+import FormButton from "../ui/FormButton";
 
-export default function LoginForm() {
+export default function SignUpForm() {
   return (
     <form class="bg-white px-5 py-6 rounded-xl">
-      <h1 className="text-2xl font-bold mb-8">Login</h1>
+      <h1 className="text-2xl font-bold mb-8">Sign Up</h1>
       <div className="mb-8">
         <div className="flex flex-col gap-4 mb-8">
+          <FormInput type="text" label="Name" name="name" />
           <FormInput type="email" label="Email" name="email" />
           <FormInput type="password" label="Password" name="password" />
         </div>
-        <FormButton type="submit">Login</FormButton>
+        <FormButton type="submit">Create Account</FormButton>
       </div>
       <p className="text-center text-primary-grey-dark">
-        Need to create an account?{" "}
+        Already have an account?{" "}
         <Link
-          href="sign-up"
+          href="login"
           className="font-bold underline text-primary-grey-darkest"
         >
-          Sign Up
+          Login
         </Link>
       </p>
     </form>
