17bca8dcb673603745296d1987991fdc97cee0a3
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-15 15:03:02 +0100

    added colours to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,8 +8,32 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // Primary colors
+        "primary-beige-dark": "var(--primary-beige-dark)",
+        "primary-beige-light": "var(--primary-beige-light)",
+        "primary-grey-darkest": "var(--primary-grey-darkest)",
+        "primary-grey-dark": "var(--primary-grey-dark)",
+        "primary-grey": "var(--primary-grey)",
+        "primary-grey-light": "var(--primary-grey-light)",
+
+        // Secondary colors
+        "secondary-green": "var(--secondary-green)",
+        "secondary-yellow": "var(--secondary-yellow)",
+        "secondary-cyan": "var(--secondary-cyan)",
+        "secondary-navy": "var(--secondary-navy)",
+        "secondary-red": "var(--secondary-red)",
+        "secondary-purple": "var(--secondary-purple)",
+
+        // Theme colors
+        "theme-purple": "var(--theme-purple)",
+        "theme-turquoise": "var(--theme-turquoise)",
+        "theme-brown": "var(--theme-brown)",
+        "theme-magenta": "var(--theme-magenta)",
+        "theme-blue": "var(--theme-blue)",
+        "theme-grey": "var(--theme-grey)",
+        "theme-army-green": "var(--theme-army-green)",
+        "theme-gold": "var(--theme-gold)",
+        "theme-orange": "var(--theme-orange)",
       },
     },
   },
