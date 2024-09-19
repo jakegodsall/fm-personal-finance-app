31db07035edb137713a9ced03c72bf52cd48422b
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-19 20:39:59 +0100

    added enums to models

--- backend/finance_tracker/models.py
+++ backend/finance_tracker/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 
+class ThemeChoices(models.TextChoices):
+    PURPLE = 'purple', 'Purple'
+    TURQUOISE = 'turquoise', 'Turquoise'
+    BROWN = 'brown', 'Brown'
+    MAGENTA = 'magenta', 'Magenta'
+    BLUE = 'blue', 'Blue'
+    GREY = 'grey', 'Grey'
+    ARMY_GREEN = 'army_green', 'Army Green'
+    GOLD = 'gold', 'Gold'
+    ORANGE = 'orange', 'Orange'
+
+
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -12,7 +24,7 @@ class Budget(BaseModel):
     maximum_spend = models.DecimalField(max_digits=10, decimal_places=2)
     start_date = models.DateField()
     end_date = models.DateField()
-    theme = models.CharField()
+    theme = models.CharField(max_length=20, choices=ThemeChoices.choices, default=ThemeChoices.PURPLE)
 
     def __str__(self):
         return self.name
@@ -22,7 +34,10 @@ class Pot(BaseModel):
     name = models.CharField(max_length=128, null=False, blank=False)
     goal_amount = models.DecimalField(max_digits=10, decimal_places=2)
     current_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    theme = models.CharField()
+    theme = models.CharField(max_length=20, choices=ThemeChoices.choices, default=ThemeChoices.PURPLE)
+
+    def __str__(self):
+        return self.name
 
 
 class RecurringBill(BaseModel):
@@ -32,10 +47,30 @@ class RecurringBill(BaseModel):
         MONTHLY = 'M', 'Monthly'
         YEARLY = 'Y', 'Yearly'
 
+    class IconChoices(models.TextChoices):
+        ARROW = 'arrow', 'Arrow'
+        BIRDS = 'birds', 'Birds'
+        BREAD = 'bread', 'Bread'
+        CIRCLE_DOT = 'circle-dot', 'Circle Dot'
+        CLOUD = 'cloud', 'Cloud'
+        EASEL = 'easel', 'Easel'
+        EGG = 'egg', 'Egg'
+        FILM = 'film', 'Film'
+        HEX = 'hex', 'Hex'
+        RADAR = 'radar', 'Radar'
+        SPOON = 'spoon', 'Spoon'
+        STAIRS = 'stairs', 'Stairs'
+        STEERING_WHEEL = 'steering-wheel', 'Steering Wheel'
+        STRING = 'string', 'String'
+        TRIANGLE = 'triangle', 'Triangle'
+
     name = models.CharField(max_length=128, null=False, blank=False)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     frequency = models.CharField(max_length=1, choices=FrequencyChoices, default=FrequencyChoices.MONTHLY, null=False)
     next_due_date = models.DateField()
-    theme = models.CharField()
-    icon = models.CharField()
+    theme = models.CharField(max_length=20, choices=ThemeChoices.choices, default=ThemeChoices.PURPLE)
+    icon = models.CharField(max_length=20, choices=IconChoices.choices, default=IconChoices.ARROW)
+
+    def __str__(self):
+        return self.name
 
